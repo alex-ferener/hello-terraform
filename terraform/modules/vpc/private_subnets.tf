242e4c99cca1b16804c15bdd3b1e0e10da9b647c
Alex Ferener <alex.ferener@gmail.com>
2022-02-09 21:26:24 +0100

    fix typo in route table (for the private subnet)

--- terraform/modules/vpc/private_subnets.tf
+++ terraform/modules/vpc/private_subnets.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw[each.key].id
+    nat_gateway_id = aws_nat_gateway.nat_gw[each.value.az].id
   }
 
   tags = merge(var.tags, {
