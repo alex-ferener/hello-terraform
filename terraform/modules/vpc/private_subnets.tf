8b576d6c06adcc43d3d2bedff7f597fe7f9165a8
Alex Ferener <alex.ferener@gmail.com>
2022-02-09 19:57:29 +0100

    fix route for private subnets (remove hardcoded value)

--- terraform/modules/vpc/private_subnets.tf
+++ terraform/modules/vpc/private_subnets.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw["eu-central-1a"].id
+    nat_gateway_id = aws_nat_gateway.nat_gw[each.key].id
   }
 
   tags = merge(var.tags, {
